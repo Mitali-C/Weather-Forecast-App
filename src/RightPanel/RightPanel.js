ab30a6541435dab6903d1d71265ffc38b6773d4f
Mitali-C <mitalichaudhari098@gmail.com>
2019-07-21 21:56:42 +0530

    error msg overlay added

--- src/RightPanel/RightPanel.js
+++ src/RightPanel/RightPanel.js
@@ -13,6 +13,7 @@ class RightPanel extends React.Component {
         weatherCondition:'',
         city: 'Pune',
         country: 'IN',
+        error: '',
     }
   }
 
@@ -23,49 +24,74 @@ class RightPanel extends React.Component {
   fetchCurrentWeather = () => {
     const { city } = this.state;
     fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&APPID=${API_KEY}`)
-    .then(response => response.json())
+    .then(response => {
+      if(!response.ok){
+        throw Error(response.statusText);
+      }
+      else{
+        return response.json()
+      }
+    })
     .then(data => {
       this.setState({
         currentTemp: data.main.temp,
         weatherCondition: data.weather[0].main,
         country: data.sys.country,
       })
+      this.props.returnCity(this.state.city, this.state.country);
     })
+    .catch((e) =>{this.setState({error: e}); console.log(e)});
   }
 
   getCity = (e) => {
     e.preventDefault();
-    this.props.returnCity(this.state.city, this.state.country);
     this.fetchCurrentWeather();
   }
 
+  onClick = () => {this.setState({error: ''});}
 
   handleChange = (e) => {
     this.setState({city: e.target.value});
   }
 
   render(){
-    const {currentTemp, date, weatherCondition, city} = this.state;
+    const {currentTemp, date, weatherCondition, city, error} = this.state;
     return(
-      <div className='rightPanelParent'>
-        <div className='heading'>WEATHER APP</div>
-        <form onSubmit={this.getCity}>
-          <input 
-            type='text' 
-            placeholder='Enter your city' 
-            value={city} 
-            className='input' 
-            onChange={this.handleChange}/>
-          <button type='submit' className='searchBtn'>Search</button>
-        </form>
-        <div className='currentWeatherHeading'>CURRENT WEATHER</div>
-        <div className='data'>{date}</div>
-        <div className='dataLabel'>Date</div>
-        <div className='data'>{currentTemp} &#x2103;</div>
-        <div className='dataLabel'>Current Temperature</div>
-        <div className='data'>{weatherCondition}</div>
-        <div className='dataLabel'>Weather Condition</div>
-      </div>
+      <React.Fragment>
+        {
+          error !== '' ? (
+            <div className='overlay' onClick={this.onClick}>
+              <div className='dialogBox'>
+                Oops! We could not find the entered city. Please try re-entering the city name.
+                <button className='closeBtn' onClick={this.onClick}>close</button>
+              </div>
+              
+            </div>
+          ) : (
+            false
+          )        
+        }
+        <div className='rightPanelParent'>
+          <div className='heading'>WEATHER APP</div>
+          <form onSubmit={this.getCity}>
+            <input 
+              type='text' 
+              placeholder='Enter your city' 
+              value={city} 
+              className='input' 
+              onChange={this.handleChange}/>
+            <button type='submit' className='searchBtn'>Search</button>
+          </form>
+          <div className='currentWeatherHeading'>CURRENT WEATHER</div>
+          <div className='data'>{date}</div>
+          <div className='dataLabel'>Date</div>
+          <div className='data'>{currentTemp} &#x2103;</div>
+          <div className='dataLabel'>Current Temperature</div>
+          <div className='data'>{weatherCondition}</div>
+          <div className='dataLabel'>Weather Condition</div>
+        </div>
+        
+      </React.Fragment>
     )
   }
 }
